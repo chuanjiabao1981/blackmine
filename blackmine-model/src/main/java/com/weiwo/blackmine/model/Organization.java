39c39fbab01512ee0a9eb9b1e861fee5e2e6e079
guowei <chuanjiabao1981@gmail.com>
2014-02-08 17:23:00 +0800

    first commit

---
+++ blackmine-model/src/main/java/com/weiwo/blackmine/model/Organization.java
@@ -0,0 +1,8 @@
+package com.weiwo.blackmine.model;
+
+public class Organization {
+
+	private String name;
+	private String contactPerson;
+	
+}
---
+++ blackmine-wechat-service/src/main/java/org/blackmine/wechat/service/home/Home.java
@@ -0,0 +1,28 @@
+package org.blackmine.wechat.service.home;
+
+import org.blackmine.wechat.tools.SignUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Home {
+	
+	private String WECHAT_TOKEN="meiqitong20140108123456";
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseBody
+	public String index(@RequestParam(value="signature") String signature,
+						@RequestParam(value="timestamp") String timestamp,
+						@RequestParam(value="nonce")	 String nonce,
+						@RequestParam(value="echostr")   String echostr)
+	{
+		
+		if (SignUtil.checkSignature(signature, WECHAT_TOKEN, timestamp, nonce)){
+			return echostr;
+		}
+		return "";
+	}
+
+}
