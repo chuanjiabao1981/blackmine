c673ede61a0625d36f89b537812b8e6845fef4a6
guowei <chuanjiabao1981@gmail.com>
2014-02-11 16:25:32 +0800

    add tools mvn module

--- blackmine-wechat-service/src/main/java/org/blackmine/wechat/service/weixin/WeiXinController.java
+++ blackmine-wechat-service/src/main/java/org/blackmine/wechat/service/weixin/WeiXinController.java
@@ -1,8 +1,9 @@
 package org.blackmine.wechat.service.weixin;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,7 +12,7 @@ public class WeiXinController {
 	private static final Logger logger = LoggerFactory.getLogger(WeiXinController.class);
 
 	@RequestMapping(value="/weixin")
-	public String weiXing(HttpRequest httpRequest)
+	public String weiXing(HttpServletRequest httpServletRequest)
 	{
 		logger.info("receive.........");
 		return "";
